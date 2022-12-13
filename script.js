d8a9ddc35d9617ebeb20229e85ca7c9c726d3d7d
miwon <ayman.shade99@gmail.com>
2022-12-13 14:48:37 +0200

    now you can see what computer choice as a text

--- script.js
+++ script.js
@@ -1,52 +1,96 @@
-
+//stats
 let humanScore=0;
 let computerScore=0;
 let round=0;
+let playerchice="";
+let computerchice="";
 
+// const imgrock = document.createElement('img')
+// const imgpapper = document.createElement('img')
+// const imgscissors = document.createElement('img')
+// imgrock.src='./imgs/rock.png';
+// imgpapper.src='./imgs/mail.png';
+// imgscissors.src='./imgs/cut.png';
+
+//selectors
+const hScore = document.querySelector('.humanScore');
+const cScore = document.querySelector('.computerScore');
+const reseting = document.querySelector('.reset');
+const Rock = document.querySelector('.rock');
+const Paper = document.querySelector('.paper');
+const Scissors = document.querySelector('.scissors')
+const pcchoice= document.querySelector('.pcchoice')
+const human = document.querySelector('.human');
+const computer = document.querySelector('.computer');
+
+
+
+//functions
 function getComputerChoice () { //function to return a random word(rock paper scissors)
     let min = 1;
     let max = 3;
     let rando = Math.floor(Math.random() * (max - min + 1)) + min;
     if (rando ==1 ) {
-        
+        // pcchoice.appendChild(imgrock);
         return "ROCK";        
     }
     if (rando == 2) {
+        // pcchoice.appendChild(imgpapper);
+
         return "PAPER";
     } else {
+        // pcchoice.appendChild(imgscissors);
         return "SCISSORS" ;
     }
+
 }
-let reset =()=>{
+
+let reset =()=>{ //the reset button
     humanScore =0 ;
     computerScore=0;
+    playerchice="";
+    computerchice="";
     update();
 }
 function update()
 {
     hScore.textContent = humanScore;
     cScore.textContent= computerScore;
+    human.textContent=playerchice;
+    computer.textContent=computerchice;
+    pcchoice.toggleAttribute('pcchoice')
+    pcchoice.toggleAttribute('pcchoice')
+
+    pcchoice.removeChild(imgrock)
+    pcchoice.removeChild(imgpapper)
+    pcchoice.removeChild(imgscissors)
 }
 
 
-
-function playRound(playerInbut , computerInbut = getComputerChoice()) {
+function playRound(playerInbut , computerInbut = getComputerChoice()) {//starts the game
 
     userinut = playerInbut.toUpperCase();
 
     if (userinut == computerInbut) {
-
-        return "Draw";
+        playerchice = userinut;
+        computerchice=computerInbut;
+        update();
     }
     
     if(userinut == "PAPER" & computerInbut == "ROCK")
     {
         humanScore++;
+        playerchice = userinut;
+        computerchice=computerInbut;
+        
         update();
     }
     if(userinut == "PAPER" & computerInbut == "SCISSORS")
     {
         computerScore++;
+        playerchice = userinut;
+        computerchice=computerInbut;
+        
         update();
 
     }  
@@ -54,11 +98,15 @@ function playRound(playerInbut , computerInbut = getComputerChoice()) {
     if(userinut == "ROCK" & computerInbut == "SCISSORS")
     {
         humanScore++;
+        playerchice = userinut;
+        computerchice=computerInbut;
         update();
     }
     if(userinut == "ROCK" & computerInbut == "PAPER")
     {
         computerScore++;
+        playerchice = userinut;
+        computerchice=computerInbut;
         update();
     }
 
@@ -66,37 +114,29 @@ function playRound(playerInbut , computerInbut = getComputerChoice()) {
     if(userinut == "SCISSORS" & computerInbut == "PAPER")
     {
         humanScore++;
+        playerchice = userinut;
+        computerchice=computerInbut;
         update();
     }   
     if(userinut == "SCISSORS" & computerInbut == "ROCK")
     {
         computerScore++;
+        playerchice = userinut;
+        computerchice=computerInbut;
         update();
     }
     else{
         return  "enter a proper value";
     }
 }
-
-//viewing the schore and reseting
-const hScore = document.querySelector('.humanScore');
-
-hScore.textContent = humanScore;
-const cScore = document.querySelector('.computerScore');
-cScore.textContent= computerScore;
-const reseting = document.querySelector('.reset');
+//events
 reseting.addEventListener('click',reset);
-
-const Rock = document.querySelector('.rock');
-const Paper = document.querySelector('.paper');
-const Scissors = document.querySelector('.scissors')
-
-
-
 Rock.addEventListener('click',()=> playRound("ROCK"));
 Paper.addEventListener('click',()=> playRound("paper"));
 Scissors.addEventListener('click',()=> playRound("scissors"));
 
+
+humanScore.addEventListener(humanScore==5,()=>{human.textContent='victory'; reset();})
 
 // const playerSelection = prompt("Enter ROCK PAPER or SCISSORS") ;
 // const computerSelection = getComputerChoice();
